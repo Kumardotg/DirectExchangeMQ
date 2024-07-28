0d6f95e0a30ebe451b5da40c8bd364e1e59fbc31
Abhinay kumar Gunna <abhinaykumargunna@Abhinays-MBP.lan>
2024-07-28 16:49:37 -0400

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+var directExchange = "direct_Exchange";
+var queueName = "ConsumerQueue";
+
+channel.QueueDeclare(queueName, false, false, false, null);
+
+var routingKeys = "letterbox,letterbox1".Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+foreach(var key in routingKeys)
+channel.QueueBind(queueName,directExchange,key);
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received => {message}");
+    if (message.Contains("Exception"))
+    {
+        Console.WriteLine("Error in Processing");
+        channel.BasicReject(ea.DeliveryTag, false);
+        throw new Exception("Error in Processing");
+    }
+    if (int.TryParse(message, out var delay))
+    {
+        Thread.Sleep(delay * 1000);
+    }
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+var directExchange = "direct_Exchange";
+channel.ExchangeDeclare(directExchange, ExchangeType.Direct, false, false, null);
+
+var routingKey = "letterbox";
+var message = "Test";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(directExchange, routingKey, null, encodedMessage);
+
+Console.WriteLine($"Published Message: {message}");
